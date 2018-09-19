d94c1be7069f78f690581a313bfcf46d10d9a5dd
Ruby <ngoc@tinypulse.com>
2018-09-19 11:39:39 +0700

    lock the record after it is approved

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,11 @@
 class PostPolicy < ApplicationPolicy
   def update?
-    record.user_id == user.id || admin_types.include?(user.try(:type))
+    return true if admin?
+    return true if record.user_id == user.id && !record.approved?
+  end
+
+  private
+  def admin?
+    admin_types.include?(user.try(:type))
   end
 end
--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 describe 'navigation' do
   let(:admin_user) { FactoryGirl.create(:admin_user) }
-  
-  before do
-    login_as(admin_user, scope: :user)
-  end
+  let(:user) { FactoryGirl.create(:user) }
 
   describe 'edit' do
     before do
@@ -13,6 +10,7 @@ describe 'navigation' do
     end
 
     it 'has a status can be edited on the form by admin' do
+      login_as(admin_user, scope: :user)
       visit edit_post_path(@post)
       choose 'post_status_approved'
       click_on 'Save'
@@ -21,13 +19,19 @@ describe 'navigation' do
     end
 
     it 'cannot be edited by a non admin' do
-      logout(:user)
-      user = FactoryGirl.create(:user)
       login_as(user, scope: :user)
 
       visit edit_post_path(@post)
 
       expect(page).to_not have_content('Approved')
+    end
+
+    it 'cannot to be edited by owner after it is approved' do
+      login_as(user, scope: :user)
+      post = Post.create(rationale: 'rationale', date: Date.today, user_id: user.id)
+      post.approved!
+      visit(edit_post_path(post))
+      expect(current_path).to eq root_path
     end
   end
 end
