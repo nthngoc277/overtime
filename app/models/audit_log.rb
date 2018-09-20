cc647fea439b8caf5adaca7fe368949a18f4bf37
Ruby <ngoc@tinypulse.com>
2018-09-20 23:01:25 +0700

    Adding spec for create and validation of audit_logs

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,12 @@
 class AuditLog < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :status, :start_date
+
+  after_initialize :set_start_date
+
+  private
+  def set_start_date
+    self.start_date ||= Date.today - 6.days
+  end
 end
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+    it 'can be properly created' do
+      audit_log = FactoryGirl.build_stubbed(:audit_log)
+      expect(audit_log).to be_valid
+    end
+  end
+
+  describe 'validations' do
+    before do
+      @audit_log = FactoryGirl.build_stubbed(:audit_log)
+    end
+
+    it 'requires to have a user association' do
+      @audit_log.user_id = nil
+      expect(@audit_log).not_to be_valid
+    end
+
+    it 'always have a status' do
+      @audit_log.status = nil
+      expect(@audit_log).not_to be_valid
+    end
+
+    it 'requires to have a start_date' do
+      @audit_log.start_date = nil
+      expect(@audit_log).not_to be_valid
+    end
+
+    it 'has a start_date equal to 6 days prior' do
+      audit_log = AuditLog.new
+      expect(audit_log.start_date).to eq(Date.today - 6.days)
+    end
+  end
 end
