b9c0054f0f6becad14b60a1b2f3083a4f9ff7b67
Ruby <ngoc@tinypulse.com>
2018-09-20 22:24:33 +0700

    Update seed data and create factory girl for audit log

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-user = User.create!(email: 'user@test.com', password: '123456', password_confirmation: '123456', first_name: 'Ngoc', last_name: 'Ng', phone_number: '967714977')
-User.create!(email: 'user1@test.com', password: '123456', password_confirmation: '123456', first_name: 'User1', last_name: 'Ng', phone_number: '967714977')
-AdminUser.create!(email: 'admin@nguyen.com', password: '123456', password_confirmation: '123456', first_name: 'Admin', last_name: 'Ng', phone_number: '967714977')
+user = User.create!(email: 'user@test.com', password: '123456', password_confirmation: '123456', first_name: 'Ngoc', last_name: 'Ng', phone_number: '0967714977')
+User.create!(email: 'user1@test.com', password: '123456', password_confirmation: '123456', first_name: 'User1', last_name: 'Ng', phone_number: '0967714977')
+AdminUser.create!(email: 'admin@nguyen.com', password: '123456', password_confirmation: '123456', first_name: 'Admin', last_name: 'Ng', phone_number: '0967714977')
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post + 1} rationale content", user_id: user.id, overtime_hours: 2.5)
