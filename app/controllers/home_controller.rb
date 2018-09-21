481e1ea21f6e9b911c3fd8670a3f991a7bed7e55
Ruby <ngoc@tinypulse.com>
2018-09-22 00:06:26 +0700

    Update seed data, display pending approval items for admin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @pending_approvals = Post.submitted
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,12 @@ user = User.create!(email: 'user@test.com', password: '123456', password_confirm
 User.create!(email: 'user1@test.com', password: '123456', password_confirmation: '123456', first_name: 'User1', last_name: 'Ng', phone_number: '0967714977')
 AdminUser.create!(email: 'admin@nguyen.com', password: '123456', password_confirmation: '123456', first_name: 'Admin', last_name: 'Ng', phone_number: '0967714977')
 
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post + 1} rationale content", user_id: user.id, overtime_hours: 2.5)
+30.times do |post|
+  Post.create!(date: Date.today, user_id: user.id, overtime_hours: 2.5, rationale: "#{post + 1} It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).  t")
 end
-puts "100 Posts have been created"
+puts "30 Posts have been created"
 
-100.times do
+30.times do
   AuditLog.create!(user_id: user.id, status: 0, start_date: Date.today - 6.days)
 end
-puts "100 Audit Logs have been created"
+puts "30 Audit Logs have been created"
