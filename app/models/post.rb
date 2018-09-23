dd7fa031dfcf3fdbf28734f0abe836f6b0c2187b
Ruby <ngoc@tinypulse.com>
2018-09-24 00:03:08 +0700

    Adding callback to confirm audit log

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,12 @@ class Post < ApplicationRecord
   def self.posts_displayed_for(user)
     user.try(:type) == 'AdminUser' ? Post.all : Post.posts_by(user)
   end
+
+  after_save :confirm_audit_log
+
+  private
+  def confirm_audit_log
+    audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date..7.days.ago)).last
+    audit_log.confirmed! if audit_log.present?
+  end
 end
